2497155b2236ec46b945ade96b2c2e801fb78fda
Eshan-Jogwar <eshanjogwar2@gmail.com>
2026-09-03 17:06:59 +0530

    added and checkted the object store is working

---
+++ cmd/server/src/main.rs
@@ -0,0 +1,29 @@
+use document::storage::{object_store::ObjectStore, s3_object_store::S3ObjectStore};
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    let store = S3ObjectStore::new(
+        "bucket".to_string(),
+        "us-east-1".to_string(),
+        "http://localhost:9000",
+        "2UAQyY3L5dlvDNDV83F3".to_string(),
+        "EhQKgnFnOlVs747cimPso5DRUhFzBC2VcYCZm6if".to_string(),
+    );
+
+    let key = "test/hello.txt";
+
+    let put_url = store
+        .presigned_put_url(key, Duration::from_secs(300))
+        .await
+        .expect("failed to generate PUT URL");
+
+    let get_url = store
+        .presigned_get_url(key, Duration::from_secs(300))
+        .await
+        .expect("failed to generate GET URL");
+
+    println!("PUT URL:\n{}\n", put_url);
+    println!("GET URL:\n{}\n", get_url);
+}
+
